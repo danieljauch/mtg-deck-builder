9ebe5f7019704ffaa0aa58f42c72115c7f4f092a
danieljauch <daniel.p.jauch@gmail.com>
2018-01-14 19:05:45 +1300

    Hid modal by default, gave default step visual settings, working on step switch

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import MoreInfoModal from './js/modals/MoreInfoModal.js';
 import SiteHeader from './js/SiteHeader.js';
 import SiteFooter from './js/SiteFooter.js';
 import Step from './js/Step.js';
+import StepChoice from './js/step/StepChoice.js';
 
 
 export default class App extends Component {
@@ -53,12 +54,12 @@ export default class App extends Component {
         </section>
         <SiteHeader siteTitle="MTG Deck Constructor" logoURL="" />
         <main>
-          <Step stepNumber="1" stepName="Format" subHeaderText="" stepContent="Choose a format below. Hover for more information." />
-          <Step stepNumber="2" stepName="Deck Type" subHeaderText="Choose a deck archetype. Hover for more information." stepContent="" />
-          <Step stepNumber="3" stepName="Lands" subHeaderText="" stepContent="" />
-          <Step stepNumber="4" stepName="Creatures" subHeaderText="" stepContent="" />
-          <Step stepNumber="5" stepName="Spells" subHeaderText="" stepContent="" />
-          <Step stepNumber="6" stepName="Print" subHeaderText="" stepContent="" />
+          <Step stepNumber="1" stepName="Format" subHeaderText="" stepContent="Choose a format below. Hover for more information." visible={true} />
+          <Step stepNumber="2" stepName="Deck Type" subHeaderText="Choose a deck archetype. Hover for more information." stepContent="" visible={false} />
+          <Step stepNumber="3" stepName="Lands" subHeaderText="" stepContent="" visible={false} />
+          <Step stepNumber="4" stepName="Creatures" subHeaderText="" stepContent="" visible={false} />
+          <Step stepNumber="5" stepName="Spells" subHeaderText="" stepContent="" visible={false} />
+          <Step stepNumber="6" stepName="Print" subHeaderText="" stepContent="" visible={false} />
         </main>
         <SiteFooter creditLink="https://danieljauch.bitbucket.io/" versionNumber="v0.2.0" />
       </div>
--- src/js/Step.js
+++ src/js/Step.js
@@ -6,10 +6,12 @@ export default class Step extends Component {
 		let { stepNumber,
 			stepName,
 			subHeaderText,
-			stepContent } = this.props;
+			stepContent,
+			visible } = this.props;
+		let classes = visible ? 'step active' : 'step';
 
 		return (
-			<section className="step">
+			<section className={classes}>
 				<header className="step-header">
 					<h2>Step #{stepNumber}: {stepName}</h2>
 					<div className="sub-header">{subHeaderText}</div>
